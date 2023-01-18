70d4b68e6b4079c518971e26f4228055fa396f83
PhuNope <mrphu2001@gmail.com>
2023-01-18 09:32:56 +0700

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let clientNo = 0;
 let roomNo;
 let hostName;
 let playerName;
-let clientReady = 0;
+let roomStatus = [];
 
 io.on("connection", (socket) => {
     //server thông báo cho clients vừa join
@@ -22,9 +22,10 @@ io.on("connection", (socket) => {
         if (clientNo % 2 != 0) {
             roomNo = socket.id;
             hostName = dataObj.data.playerName;
-
             socket.join(roomNo);
             data.data = { roomName: roomNo };
+
+            roomStatus[roomNo] = 0;
             //server gửi thông báo room host cho clients
             socket.emit(process.env.YOU_ARE_HOST, data);
         } else {
@@ -38,16 +39,46 @@ io.on("connection", (socket) => {
 
     //nhận thông báo sẵn sàng của clients
     socket.on(process.env.CLIENT_READY, (data) => {
-        clientReady++;
-        if (clientReady == 1) {
-            dataObj.data = { roomName: data.data.roomName, status: false };
-            io.to(data.data.roomName).emit(process.env.ALL_CLIENTS_ARE_READY, dataObj);
+        roomStatus[data.data.roomName]++;
+
+        if (roomStatus[data.data.roomName] == 1) {
+            dataObj.data = { roomName: data.data.roomName, playerName: data.data.playerName };
+            io.to(data.data.roomName).emit(process.env.CLIENT_READY, dataObj);
         }
 
-        if (clientReady == 2) {
-            clientReady = 0;
+        if (roomStatus[data.data.roomName] == 2) {
             dataObj.data = { roomName: data.data.roomName, status: false };
             io.to(data.data.roomName).emit(process.env.ALL_CLIENTS_ARE_READY, dataObj);
         }
+    });
+
+    //gửi và nhận thông tin clients hủy sẳn sàng
+    socket.on(process.env.CLIENT_UNREADY, (data) => {
+        roomStatus[data.data.roomName]--;
+
+        dataObj.data = { roomName: data.data.roomName, playerName: data.data.playerName };
+        io.to(data.data.roomName).emit(process.env.CLIENT_UNREADY, dataObj);
+    });
+
+    //nhận thông tin đạn từ client
+    socket.on(process.env.CLIENTS_FIRE, (data) => {
+        io.to(data.data.roomName).emit(process.env.CLIENTS_FIRE, data);
+    });
+
+    //nhận trạng thái bắn trúng
+    socket.on(process.env.IS_HIT, (data) => {
+        io.to(data.data.roomName).emit(process.env.IS_HIT, data);
+
+        if (!data.data.isHit) {
+            //gửi client turn tiếp theo
+            dataObj.data = { playerName: data.data.playerName, roomName: data.data.roomName };
+            io.to(data.data.roomName).emit(process.env.NEXT_TURN, dataObj);
+        }
+    });
+
+    //Gửi lên server hết tàu
+    socket.on(process.env.DIE_ALL_SHIP, (data) => {
+        //gửi thông báo người win
+        io.to(data.data.roomName).emit(process.env.WIN_GAME, dataObj);
     });
 });
